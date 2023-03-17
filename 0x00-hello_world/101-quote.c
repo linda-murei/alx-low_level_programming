feb36ecc9dca2178cef96a64d225f1e815543186
linda-murei <lindajepleting@gmail.com>
2023-03-16 23:19:30 -0700

    Intel

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - the main function
+ *
+ * Return: after completion 1
+ */
+int main(void)
+{
+	fput("and that piece of art is useful"\" -Dora Korpar, 2015-10-19\n, stdout");
+	return (1);
+}
