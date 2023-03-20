dd068d9e638026606f4b4451f7f8a718ca638c2a
linda-murei <lindajepleting@gmail.com>
2023-03-20 04:02:22 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints lowercase alphabet in reverse
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter >= 'a' ; letter--)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
+
