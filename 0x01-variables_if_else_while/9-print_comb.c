785e6090dc7e90acae0ebd7d669e0a4062c564c6
linda-murei <lindajepleting@gmail.com>
2023-03-20 04:38:43 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combination of single digit numbers
+ * *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i = '0';
+
+	while (i <= '9')
+	{
+		putchar(i);
+		if (i != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		++i;
+	}
+	putchar('\n');
+	return (0);
+}
+
