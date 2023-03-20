1161a96beffd8dbaa9f7737c0fde7e8bbca4fe9c
linda-murei <lindajepleting@gmail.com>
2023-03-20 02:31:46 -0700

    When I was having that alphabet soup, I never thought that it would pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * Main - the entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+int lowercase = 'a';
+while (lowercase <= 'z')
+{
+if (lowercase == 'e' || lowercase == 'q')
+{
+lowercase += 1;
+}
+else
+{
+putchar(lowercase);
+lowercase += 1;
+}
+}
+putchar('\n');
+return (0);
+}
