d0b1e78431ffd8662aa5c8c76d5b2787a6d1593f
linda-murei <lindajepleting@gmail.com>
2023-03-27 05:51:29 -0700

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - function that returns length of a string
+ * @a: input
+ * @b: input
+ * Return:n
+ */
+void swap_int(int *a, int *b)
+{
+	int m;
+
+	m = *a;
+	*a = *b;
+	*b = m;
+}
+
