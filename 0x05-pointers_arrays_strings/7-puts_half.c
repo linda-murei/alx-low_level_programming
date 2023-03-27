de496f58f9cec240fac919d4afc76cfa0856339b
linda-murei <lindajepleting@gmail.com>
2023-03-27 09:49:25 -0700

    Winning is only half of it. Having fun is the other half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,19 +8,19 @@
 void puts_half(char *str)
 {
 	int i;
-	int length = 0;
+	int len = 0;
 
 	for (i = 0 ; str[i] != '\0' ; i++)
-		length++;
-	if (length % 2 == 0)
+		len++;
+	if (len % 2 == 0)
 	{
-		for (i = length / 2 ; str[i] != '\0' ; i++)
+		for (i = len / 2 ; str[i] != '\0' ; i++)
 		_putchar(str[i]);
 		_putchar('\n');
 	}
-	else if (length % 2 != 0)
+	else if (len % 2 != 0)
 	{
-	for (i = (length - 1) / 2 ; str[i] != '\0' ; i++)
+	for (i = (len - 1) / 2 ; str[i] != '\0' ; i++)
 	{
 		_putchar(str[i]);
 	}
