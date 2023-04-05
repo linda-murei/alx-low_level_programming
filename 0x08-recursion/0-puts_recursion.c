c2969a1491960ef260c22db6856527552c6e3dc1
linda-murei <lindajepleting@gmail.com>
2023-04-05 05:00:59 -0700

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,8 +1,17 @@
 #include "main.h"
 /**
- * _puts_recursion - prints a string, followed by a new line.
- * @s: string to be printed.
+ *  _puts_recursion - prints a new string.
+ *  @s: string to be printed.
  */
 
-void _puts_recursion(char *s)
 
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+}
