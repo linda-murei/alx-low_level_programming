fee09c6c89d0802f1c59c6b119682892a2924471
linda-murei <lindajepleting@gmail.com>
2023-04-15 03:36:34 -0700

    Neither irony nor sarcasm is argument

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the multiplication of two arguments
+ * @argc: the number or arguments
+ * @argv: pointer of pointer of char
+ *        represents the arguments list
+ * Return: 0 (success_code)
+ */
