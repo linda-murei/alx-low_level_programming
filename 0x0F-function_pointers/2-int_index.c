159298d533800d6556cdde869cfd3366192438fa
linda-murei <lindajepleting@gmail.com>
2023-04-19 10:11:54 -0700

    To hell with circumstances

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -17,7 +17,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	return (-1);
 	for (index = 0; index < size; index++)
 	{
-	if (cmp(array[index]) != 0)
+	if (cmp(array[index]))
 			return (index);
 	}
 	return (-1);
